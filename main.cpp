e1a2eb0ee338ad228074d1086159001fcdee4fe0
SifisoYende <stevensifiso65@gmail.com>
2026-07-07 08:54:18 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,53 +1,41 @@
-/************************************************************************
-*  Author: Sifiso Yende
-*
-*  Program: The program gets the present populations of two cities A,B,
-*  where the population of A is less than of B,
-*  and their growth rates, then find out when will population A
-*  be equal or greater than of B.
-****************************************************************************/
+/***********************************************************************
+* Author: Sifiso Yende                                                 *
+*                                                                      *
+* Program: The program, takes an integer as input, then                *
+* displays if it is a prime or not prime.                              *
+************************************************************************/
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    int pop_A;
-    int pop_B;
+    int num;
 
-    double rate_A;
-    double rate_B;
+    cout << "Enter an integer:\n";
+    cin >> num;
 
-    int numOfYears = 0;
-
-    cout << "Enter the current populations of city A, and B, respectively:\n";
-    cin >> pop_A >> pop_B;
-    cout << endl;
-
-    cout << "Enter the growth rates (%) of city A, and B, respectively:\n";
-    cin >> rate_A >> rate_B;
-
-    rate_A = rate_A/100.0;
-    rate_B = rate_B/100.0;
-    cout << endl;
-
-    cout << fixed << showpoint << setprecision(2);
-    cout << "Population_A:    " << "Population_B:    " << "Years:    "<<endl;
-    cout << setw(12) << pop_A <<setw(17)<<pop_B << setw(11)<<numOfYears << endl;
-    while (pop_A < pop_B)
+    if (num == 1)
     {
-        numOfYears++;
-        pop_A += (pop_A*rate_A);
-        pop_B += (pop_B*rate_B);
-
-        cout << setw(12) << pop_A <<setw(17)<<pop_B << setw(11)<<numOfYears
-             << endl;
+        cout << "Not Prime";
+        return 1;
+    }
+    if (num ==2)
+    {
+        cout << "Prime!!!";
+        return 0;
     }
 
-    cout <<"After "<< numOfYears <<" years, "
-         << "population_A will be: " <<pop_A
-         << " greater or equal to: " <<pop_B
-         << " of population_B."<<endl;
+    for (int i =3; (i<=num/2); i++)
+    {
+        if (num%i == 0)
+        {
+            cout << "Not Prime!!!";
+            return 1;
+        }
+    }
+    cout << "Prime!!!";
+    cout << endl;
+
     return 0;
 }
